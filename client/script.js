20c56c5b433cd6ec77a12aa38f78d4fda08bd94a
anoop <anoopaathreya@gmail.com>
2022-12-25 13:42:54 +0530

    project complete

--- client/script.js
+++ client/script.js
@@ -88,7 +88,33 @@ const handleSubmit = async   (e) => {
     //to fetch newly created div
     const messageDiv = document.getElementById(uniqueID);
     
-    loader(messageDiv)
+    loader(messageDiv);
+
+    //fetch data from the server i.e bot's response
+    const response = await fetch('http://localhost:5000',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'), //textarea value
+        })
+    })
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if(response.ok){
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData);
+    }
+    else{
+        const err = await response.text();
+        messageDiv.innerHTML = "Something went wrong please try again";
+        alert(err);
+    }
 }
 
 
